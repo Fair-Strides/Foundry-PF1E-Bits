b1f8daa444f5787a27000454bbf388dca5a328bd
Fair-Strides <tristongoucher@gmail.com>
2023-02-20 18:01:22 -0800

    Updated to work with Foundry v10 and PF1E 0.82.5.

--- Dragoon_Guns/Gun_script_call.js
+++ Dragoon_Guns/Gun_script_call.js
@@ -3,14 +3,14 @@ let ammoName = 'Dragoon Cartridge';
 let theActor = !!token ? token.actor : actor;
 let ammo = theActor.items.find(o => o.type === 'loot' && o.name === ammoName);
 
-await theActor.updateEmbeddedDocuments("Item", [{_id: item.id, 'data.uses.value': item.data.data.uses.value - (attacks.length - 0)}]);
+await theActor.updateEmbeddedDocuments("Item", [{_id: item.id, 'system.uses.value': item.system.uses.value - (shared.attackData.attacks.length - 0)}]);
 
 if(ammo === undefined) {
 	ui.notifications.warn(ammoName + " not found on " + theActor.name);
 }
-else if(ammo.data.data.quantity === 0) {
+else if(ammo.system.quantity === 0) {
     ui.notifications.warn(theActor.name + " does not have any more " + ammoName);
 }
-else if (item.data.data.uses.value === 0) {
-    theActor.updateEmbeddedDocuments("Item", [{_id: item.id, "data.uses.value": 3}, {_id: ammo.id, "data.quantity": (ammo.data.data.quantity - 1)}]);
+else if (item.system.uses.value === 0) {
+    theActor.updateEmbeddedDocuments("Item", [{_id: item.id, "system.uses.value": 3}, {_id: ammo.id, "system.quantity": (ammo.system.quantity - 1)}]);
 }
